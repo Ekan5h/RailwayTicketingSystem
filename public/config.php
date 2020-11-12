bfd1c8243692fb7fe604a2285c8f406384fdd2bd
Ekansh Mahendru <ekanshmahendru@gmail.com>
2020-11-12 12:09:43 +0530

    Update config.php

--- public/config.php
+++ public/config.php
@@ -1,6 +1,6 @@
 <?php
-const DB_HOST = 'localhost';
-const DB_NAME = 'railway';
+const DB_HOST = 'arjuna.db.elephantsql.com';
+const DB_NAME = 'guxwbsyz';
 const DB_PORT = '5432';
-const DB_USERNAME = 'postgres';
-const DB_PASSWORD = 'insertPassword';
+const DB_USERNAME = 'guxwbsyz';
+const DB_PASSWORD = 'SoBIoj2gPDxvrZGpYf4XGBtHxS8W4iPx';
