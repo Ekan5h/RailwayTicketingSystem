212cee1cfcb0b3c779c865742f26752ab9fe50e7
Ekansh Mahendru <ekanshmahendru@gmail.com>
2020-12-09 15:18:58 +0530

    Update config.php

--- public/config.php
+++ public/config.php
@@ -1,12 +1,6 @@
 <?php
-// const DB_HOST = 'arjuna.db.elephantsql.com';
-// const DB_NAME = 'guxwbsyz';
-// const DB_PORT = '5432';
-// const DB_USERNAME = 'guxwbsyz';
-// const DB_PASSWORD = 'SoBIoj2gPDxvrZGpYf4XGBtHxS8W4iPx';
-
 const DB_HOST = 'localhost';
 const DB_NAME = 'cs301_demo';
 const DB_PORT = '5432';
 const DB_USERNAME = 'postgres';
-const DB_PASSWORD = '***';
+const DB_PASSWORD = '****';
